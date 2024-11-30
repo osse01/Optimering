cb5df77f40cfebd03b28f5bc5c081c3f0746c8e1
Oscar <o.jemsson@gmail.com>
2024-11-30 10:32:28 +0100

    Initial push for TAOP04

---
+++ conjugateDir/Gram_Schmidt.m
@@ -0,0 +1,17 @@
+%% min f(x) = x'Qx - b'x using conjugate directions
+% Starting with the unit vectors as start directions
+
+Q = [1   1   1;
+     1   2  -1/2;
+     0  1/2  1 ]
+b = [-1 -3 1]';
+
+xi = [[1 0 0]' [0 1 0]' [0 0 1]']; % Start with unit vectors
+d = [[1 0 0]' [0 0 0]' [0 0 0]'];       % First con. dir. as xi(1)
+
+a_21 = -xi(2)'*Q*d(1) / ( d(1)'*Q*d(1) )
+d(2) = xi(2) + a_21*d(1)
+
+a_31 = -xi(3)'*Q*d(1) / ( d(1)'*Q*d(1) )
+a_32 = -xi(3)'*Q*d(2) / ( d(2)'*Q*d(2) )
+d(3) = xi(3) + a_31*d(1) + a_32*d(2)
--- simplex/simplex.m
+++ simplex/simplex.m
@@ -3,16 +3,17 @@ function x = simplex( f, x )
 
 % Max
 [f_max,kMax] = max( f( x(1,1:end),x(2,1:end) ) );
-x_max = x(1:end,kMax)
+x_max = x(1:end,kMax);
 % Min
 [f_min,kMin] = min( f( x(1,1:end),x(2,1:end) ) );
-x_min = x(1:end,kMin)
+x_min = x(1:end,kMin);
 
 % Middle point
-[~,n] = size(x);
-x_hat = 1/n *(sum( [x(1,1:end),x(2,1:end)] ) - x_max)
+[n,~] = size(x);
+x_hat = 1/n *(sum( [x(1,1:end)', x(2,1:end)'] ) - x_max')';
+
 % Mirror point
-x_ref = x_hat + (x_hat - x_max) % Mirror point
+x_ref = 2*x_hat - x_max;
 
 
 f_high = max( f(x(1,1:end),x(2,1:end)) ~= f_max ); % The next highest f.
@@ -26,13 +27,13 @@ if ( f_ref < f_min ) % x_ref best!
     else
         x_new = x_ref;
     end
-elseif ( f_high > f_ref && f_ref >= f_min )
+elseif ( f_high > f_ref )
     x_new = x_ref;
 elseif ( f_ref >= f_high )
     if ( f_ref >= f_max )
-        x_new = 0.5 * (x_max-x_hat);
+        x_new = 0.5 * (x_max+x_hat);
     else
-        x_new = 0.5 * (x_ref + x_hat)
+        x_new = 0.5 * (x_ref + x_hat);
     end
 end
 x(1:end,kMax) = x_new;
