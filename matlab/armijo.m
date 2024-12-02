bf27361e89bed013e887ff33d7a97e5ae345140e
Adam <ah.adamhallberg@gmail.com>
2024-12-02 15:20:14 +0100

    Revised code.

--- matlab/armijo.m
+++ matlab/armijo.m
@@ -4,39 +4,34 @@ close all
 clc
 
 % Variables
-x_0 = [0, 1000]';
-x_curr = x_0;
-x_star = [2, 1]';
-epsilon = 1e-3;
-sigma = 0.5; % Must be in [0,1]
-run = true;
-vals = zeros(1e4, 1);
+x_curr = [0, 3]';       % Initial guess.
+epsilon = 1e-3;         % Error tolerance.
+sigma = 0.5;            % Must be in [0,1].
+vals = zeros(1e4, 1);   % To Store Values for Plot Later.
+iter = 1;               % Iteration Counter.
 
-% Define functions
-% Function.
+% Define functions.
 f = @(x) (x(1) - 2)^4 + (x(1) - 2*x(2))^2;
-% Gradient of function.
 g = @(x) [4*(x(1)-2)^3 + 2*(x(1) - 2*x(2)); -4*(x(1) - 2*x(2))];
+h = @(x) [12*(x(1)-2)^2 + 2, -4;-4, 8];
 
-vals(1, :) = f(x_curr);
+while (true)
+    vals(iter, :) = f(x_curr);
+    % Initial Newton Step.
+    t = (norm(g(x_curr))^2)/( g(x_curr)'*(h(x_curr)*g(x_curr) ));
 
-% Initial Newton Step.
-x_curr = x_curr - f(x_curr)/norm(g(x_curr));
-
-i = 2;
-while (run)
-    vals(i, :) = f(x_curr);
-    d = -g(x_curr);  % Descent direction
-    t = 1e0;         % Initial step size
+    % Descent Direction.
+    d = -g(x_curr);
 
     while (f(x_curr + t * d) >= f(x_curr) + sigma * g(x_curr)' * d * t)
         t = t * 0.5;  
     end
-    x_curr = x_curr + t * d;  % Update
+    % Update x.
+    x_curr = x_curr + t * d;  
     if (norm(g(x_curr)) <= epsilon)
-        run = false;
+        break;
     end
-    i = i + 1;
+    iter = iter + 1;
 end
 values = vals(1:ceil(length(find(vals))/2), :);
 iterations = linspace(1, ceil(length(find(vals))/2), ceil(length(find(vals))/2))';
@@ -46,7 +41,6 @@ disp(x_curr);
 
 % Plot values against iterations.
 disp("Plot!")
-% figure
 plot(iterations, values, '-o')
 xlabel('Iterations')
 ylabel('Values')
