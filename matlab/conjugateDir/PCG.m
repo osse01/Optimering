fdcc491e803e2fa51fbcd92c5c8c21df569b603a
Oscar <o.jemsson@gmail.com>
2024-12-09 12:09:00 +0100

    Added pre-Conditioned Conjugate Gradient Method and changed small stuff

--- matlab/conjugateDir/PCG.m
+++ matlab/conjugateDir/PCG.m
@@ -1,5 +1,5 @@
-%% Conjugate Gradient Method
-% Definine Q och b
+%% Preconditioned Conjugate Direction Method
+% Define Q, b och H
 Q = [1001, 1, 1, 1, 1;
      1, 999, 1, 1, 1;
      1, 1, 101, 1, 1;
@@ -7,10 +7,14 @@ Q = [1001, 1, 1, 1, 1;
      1, 1, 1, 1, 10];
 b = [1; 1; 1; 1; 1];
 
-% Start point och initialization
+H = diag([1/1000, 1/1000, 1/100, 1/100, 1/10]);
+S = inv(H); % Preconditioned matrix
+
+% Start point and initalization
 x_k = zeros(5, 1); % Start in origin
-d_k = b - Q * x_k; % Initial residual
-g_k = Q*x_k - b;
+g_k = b - Q * x_k; % Initial residual
+y_k = S * g_k; % Solve the preconditioned system
+d_k = y_k;
 max_iter = 100;
 tolerance = 1e-10;
 
@@ -22,11 +26,10 @@ gradient_norms = [];
 x_star = Q \ b;
 f_star = 0.5 * x_star' * Q * x_star - b' * x_star;
 
-% Iterate conjugate gradient method
+% Iterate PCG
 for k = 1:max_iter
-    k
     gradient_norm = norm(g_k);
-    f_k = 0.5 * x_k' * Q * x_k - b' * x_k
+    f_k = 0.5 * x_k' * Q * x_k - b' * x_k;
 
     % Save values
     f_values = [f_values; f_k - f_star];
@@ -38,35 +41,39 @@ for k = 1:max_iter
     end
 
     % Calc steplength
-    alpha_k = -(g_k' * d_k) / (d_k' * Q * d_k);
+    alpha_k = (g_k' * y_k) / (d_k' * Q * d_k);
 
     % Update sol
-    x_k = x_k + alpha_k * d_k
+    x_k = x_k + alpha_k * d_k;
 
-    % Update gradient
-    g_new = Q*x_k - b;
+    % Update residual
+    g_new = g_k - alpha_k * Q * d_k;
+
+    % Calc y_k+1
+    y_new = S * g_new;
 
     % Update beta
-    beta_k = (g_new' * g_new) / (g_k' * g_k)
+    beta_k = (y_new' * g_new) / (y_k' * g_k);
 
-    % Update search dir
-    d_k = -g_new + beta_k * d_k;
+    % Update search direction
+    d_k = y_new + beta_k * d_k;
 
     % Prepare next iteration
-    g_k = g_new;
+    g_k = r_new;
+    y_k = y_new;
 end
 
-% Plot results
+% Ploa results
 iterations = 1:length(f_values);
 
 figure;
 
-% Diff in function values
+% Difference in function values
 subplot(1, 2, 1);
 semilogy(iterations, f_values, '-o');
 xlabel('Iterations');
 ylabel('f(x_k) - f(x^*)');
-title('Convergence of function values');
+title('Convergence of function values (PCG)');
 grid on;
 
 % Gradient norm
@@ -74,6 +81,5 @@ subplot(1, 2, 2);
 semilogy(iterations, gradient_norms, '-o');
 xlabel('Iterations');
 ylabel('||\nabla f(x_k)||');
-title('Convergence of gradient norm');
+title('Convergence of gradient norm (PCG)');
 grid on;
-
